7fb7867189eb5746ef87b9d5e6a5c79c0f616d46
Iliya <Iliya.mi78@gmail.com>
2020-04-25 22:42:42 +0430

    Market editing

--- Market.h
+++ Market.h
@@ -14,7 +14,7 @@ public:
     bool GUI;
     bool Increasing_Server_Capacity;
     bool Lab;
-    //Setters for set values
+    /*Setters for set values*/
     void set_Security();
     void set_Herbs_Level2();
     void set_Herbs_Level3();
@@ -23,6 +23,8 @@ public:
     void set_Lab();
     //Constructor Just for set values
     Market();
+    //Buy System
+    void Buy();
 };
 
 void Market::set_Security()
@@ -59,4 +61,47 @@ Market::Market()
     set_Increasing_Server_Capacity();
     set_Lab();
 }
+
+void Market::Buy()
+{
+    cout << "\t\t\t Welcome to The Market" << endl;
+    cout << "What Do you want to buy ?" << endl;
+    cout << "(1) Level 2 goods" << endl;
+    cout << "(2) Level 3 goods" << endl;
+    cout << "(3) Security   Cost: 150,000T" << endl;
+    cout << "(4) GUI   Cost: 200,000T" << endl;
+    cout << "(5) Increasing APP's Server Capacity(more customers) Cost: 200,000T" << endl;
+    cout << "(6) Lab   Cost: 350,000T\n" << endl;
+    int choice;
+    cout << "Your choice: ";
+    cin >> choice;
+    cout << endl;
+    switch (choice)
+    {
+    case 1:
+        
+        break;
+    case 2:
+        
+        break;
+    case 3:
+        
+        break;
+    case 4:
+        
+        break;
+    case 5:
+        
+        break;
+    case 6:
+        
+        break;
+        
+    
+    default:
+        break;
+    }
+
+}
+
 #endif
