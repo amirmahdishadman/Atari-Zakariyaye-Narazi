c90122083c690ce336990a6aa9d743f404a8136f
Iliya <Iliya.mi78@gmail.com>
2020-04-26 08:55:31 +0430

    Market First initialized

--- Market.h
+++ Market.h
@@ -1,6 +1,50 @@
-#ifndef ill
-#define ill
+#ifndef Market_H
+#define Market_H
+#include <iostream>
 
+using namespace std;
 
+class Market
+{
+public:
+    /*Option that Amo Atar can buy!*/
+    bool Security;
+    bool Herbs_Level2;
+    bool Herbs_Level3;
+    bool GUI;
+    bool Increasing_Server_Capacity;
+    bool Lab;
+    //Setters for set values
+    void set_Security();
+    void set_Herbs_Level2();
+    void set_Herbs_Level3();
+    void set_GUI();
+    void set_Increasing_Server_Capacity();
+    void set_Lab();
+};
 
+void Market::set_Security()
+{
+    Security = false;
+}
+void Market::set_Herbs_Level2()
+{
+    Herbs_Level2 = false;
+}
+void Market::set_Herbs_Level3()
+{
+    Herbs_Level3 = false;
+}
+void Market::set_GUI()
+{
+    GUI = false;
+}
+void Market::set_Increasing_Server_Capacity()
+{
+    Increasing_Server_Capacity = false;
+}
+void Market::set_Lab()
+{
+    Lab = false;
+}
 #endif
