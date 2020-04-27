6fd83f9158bf3705b116f9937d788fb4a16345ef
Iliya <Iliya.mi78@gmail.com>
2020-04-27 20:32:32 +0430

    Atar Class Edited (Some funcs moved to Market class)

--- Atar.h
+++ Atar.h
@@ -1,9 +1,9 @@
-#ifndef atar
-#define atar
-#include "Market.h"
+#ifndef Atar_H
+#define Atar_H
 
 class Atar
 {
+    friend class Market;
 private:
     //Amo Atar total money
     double Money;        
@@ -14,14 +14,6 @@ public:
     void setReputation_Level(int);
     double getMoney();
     int getReputation_Level();
-    
-    /*Return True if each option in market was purchased*/
-    bool Check_Security(Market &);
-    bool Check_Herbs_Level2(Market &);
-    bool Check_Herbs_Level3(Market &);
-    bool Check_GUI(Market &);
-    bool Check_Increasing_Server_Capacity(Market &);
-    bool Check_Lab(Market &);
 };
 
 void Atar::setMoney(double money)
@@ -44,76 +36,6 @@ int Atar::getReputation_Level()
     return this->Reputation_Level;
 }
 
-bool Atar::Check_Security(Market &market)
-{
-    if (market.Security == true)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
 
-bool Atar::Check_Herbs_Level2(Market &market)
-{
-    if (market.Herbs_Level2 == true)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-bool Atar::Check_Herbs_Level3(Market &market)
-{
-    if (market.Herbs_Level3 == true)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-bool Atar::Check_GUI(Market &market)
-{
-    if (market.GUI == true)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-bool Atar::Check_Increasing_Server_Capacity(Market &market)
-{
-    if (market.Increasing_Server_Capacity == true)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-bool Atar::Check_Lab(Market &market)
-{
-    if (market.Lab == true)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
 
 #endif
