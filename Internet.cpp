42f9b4bc1fbd5e132d1c400d7d6c032949c534ac
Iliya.mi <iliya.mi78@gmail.com>
2020-06-10 04:03:35 +0430

    Internet O Done

--- Internet.cpp
+++ Internet.cpp
@@ -5,34 +5,27 @@ void Internet::internet(int level)
 {
     cout << " *------*[Internet]*------*" << endl;
 
-    cout << "~> Baraye Motalee Darbare Daro Ha Be Link Zir Morajee Konid:" << endl
-         << endl;
-    cout << " -->  https://github.com/amirmahdishadman/Drug-and-disease-information  <--" << endl;
+    cout << "Baraye Motalee Darbare Daro Ha Be Link Zir Morajee Konid:" << endl;
+    cout << "https://github.com/amirmahdishadman/Drug-and-disease-information" << endl;
     switch (level)
     {
 
     case 1:
     {
-        cout << "-----------------------------------------------------" << endl;
-        cout << "~> File 1 Password : File1.Level1.AmoAtar7249 " << endl;
-        cout << "-----------------------------------------------------" << endl;
+        cout << "File 1 Password : File1.Level1.AmoAtar7249" << endl;
         break;
     }
     case 2:
     {
-        cout << "-----------------------------------------------------" << endl;
-        cout << "~> File 1 Password : File1.Level1.AmoAtar7249" << endl;
-        cout << "~> File 2 Password : File2.Level2.AmoAtar1657" << endl;
-        cout << "-----------------------------------------------------" << endl;
+        cout << "File 1 Password : File1.Level1.AmoAtar7249" << endl;
+        cout << "File 2 Password : File2.Level2.AmoAtar1657" << endl;
         break;
     }
     case 3:
     {
-        cout << "-----------------------------------------------------" << endl;
-        cout << "~> File 1 Password : File1.Level1.AmoAtar7249" << endl;
-        cout << "~> File 2 Password : File2.Level2.AmoAtar1657" << endl;
-        cout << "~> File 3 Password : File3.Level3.AmoAtar1943" << endl;
-        cout << "-----------------------------------------------------" << endl;
+        cout << "File 1 Password : File1.Level1.AmoAtar7249" << endl;
+        cout << "File 2 Password : File2.Level2.AmoAtar1657" << endl;
+        cout << "File 3 Password : File3.Level3.AmoAtar1943" << endl;
         break;
     }
     }
