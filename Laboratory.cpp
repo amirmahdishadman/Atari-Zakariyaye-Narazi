7e42dc7488db753582278806c83c503e0412ddfa
Iliya.mi <iliya.mi78@gmail.com>
2020-06-10 06:23:48 +0430

    Lab O Done

--- Laboratory.cpp
+++ Laboratory.cpp
@@ -2,43 +2,33 @@
 
 void Labratory::Make_Alcohol(Atar &amo_atar)
 {
-    cout << "Farayande Dorost Kardan Alcohol...\n"
-         << endl;
     int C, H, O;
-    cout << "Tedade Carbon: " << endl;
+    cin.exceptions(ios_base::failbit);
     try
     {
         cin >> C;
     }
     catch (const std::ios_base::failure &failure)
     {
-        cout << "Alan Mifrestimon Hava AmoAtar, Tarkib Ghalat!" << endl;
+        cout << "Boro Boro Biron Amo Atar k Alan Mirim Ro Hava!!!" << endl;
     }
-    try
-    {
-        cin >> C;
-    }
-    catch (const std::ios_base::failure &failure)
-    {
-        cout << "Alan Mifrestimon Hava AmoAtar, Tarkib Ghalat!" << endl;
-    }
-    cout << "Tedade Hydrogen: " << endl;
+    cin.exceptions(ios_base::failbit);
     try
     {
         cin >> H;
     }
     catch (const std::ios_base::failure &failure)
     {
-        cout << "Alan Mifrestimon Hava AmoAtar, Tarkib Ghalat!" << endl;
+        cout << "Boro Boro Biron Amo Atar k Alan Mirim Ro Hava!!!" << endl;
     }
-    cout << "Tedade Oxygen: " << endl;
+    cin.exceptions(ios_base::failbit);
     try
     {
         cin >> O;
     }
     catch (const std::ios_base::failure &failure)
     {
-        cout << "Alan Mifrestimon Hava AmoAtar, Tarkib Ghalat!" << endl;
+        cout << "Boro Boro Biron Amo Atar k Alan Mirim Ro Hava!!!" << endl;
     }
     Check_Alcohol_inputs(C, H, O, amo_atar);
 }
@@ -47,8 +37,7 @@ bool Labratory::Check_Alcohol_inputs(int C, int H, int O, Atar &amo_atar)
 {
     if (C == 2 && H == 6 && O == 1)
     {
-        cout << "\t\t Payane Farayand!" << endl;
-        cout << "Shoma Yek Bottry Alcohol Dorost Kardid." << endl;
+        cout << "Shoma Yek Vahed Alcohol Dorost Kardid." << endl;
         //Amount increament in Inventory
         amo_atar.set_repository_amount("Alcohol", amo_atar.get_repository_amount("Alcohol") + 1);
 
@@ -56,42 +45,40 @@ bool Labratory::Check_Alcohol_inputs(int C, int H, int O, Atar &amo_atar)
     }
     else
     {
-        cout << "Tarkibe Eshtebah!\n Dobare Talash Konid!" << endl;
+        cout << "Tarkibe Eshtebah! Dobare Talash Konid!" << endl;
         return false;
     }
 }
 
 void Labratory::Make_Comphor(Atar &amo_atar)
 {
-    cout << "Farayande Dorost Kardan Kafoor...\n"
-         << endl;
     int C, H, O;
-    cout << "Tedade Carbon: " << endl;
+    cin.exceptions(ios_base::failbit);
     try
     {
         cin >> C;
     }
     catch (const std::ios_base::failure &failure)
     {
-        cout << "Alan Mifrestimon Hava AmoAtar, Tarkib Ghalat!" << endl;
+        cout << "Boro Boro Biron Amo Atar k Alan Mirim Ro Hava!!!" << endl;
     }
-    cout << "Tedade Hydrogen: " << endl;
+    cin.exceptions(ios_base::failbit);
     try
     {
         cin >> H;
     }
     catch (const std::ios_base::failure &failure)
     {
-        cout << "Alan Mifrestimon Hava AmoAtar, Tarkib Ghalat!" << endl;
+        cout << "Boro Boro Biron Amo Atar k Alan Mirim Ro Hava!!!" << endl;
     }
-    cout << "Tedade Oxygen: " << endl;
+    cin.exceptions(ios_base::failbit);
     try
     {
         cin >> O;
     }
     catch (const std::ios_base::failure &failure)
     {
-        cout << "Alan Mifrestimon Hava AmoAtar, Tarkib Ghalat!" << endl;
+        cout << "Boro Boro Biron Amo Atar k Alan Mirim Ro Hava!!!" << endl;
     }
     Check_Comphor_inputs(C, H, O, amo_atar);
 }
@@ -100,8 +87,7 @@ bool Labratory::Check_Comphor_inputs(int C, int H, int O, Atar &amo_atar)
 {
     if (C == 10 && H == 16 && O == 1)
     {
-        cout << "\t\t Payane Farayand!" << endl;
-        cout << "Shoma Yek Pack Kafoor Dorost Kardid." << endl;
+        cout << "Shoma Yek Vahed Kafoor Dorost Kardid." << endl;
         //Amount increament in Inventory
         amo_atar.set_repository_amount("Kafoor", amo_atar.get_repository_amount("Kafoor") + 1);
 
@@ -109,42 +95,40 @@ bool Labratory::Check_Comphor_inputs(int C, int H, int O, Atar &amo_atar)
     }
     else
     {
-        cout << "Tarkibe Eshtebah!\n Dobare Talash Konid!" << endl;
+        cout << "Tarkibe Eshtebah! Dobare Talash Konid!" << endl;
         return false;
     }
 }
 
 void Labratory::Make_Naphtaline(Atar &amo_atar)
 {
-    cout << "Farayande Dorost Kardan Naphtaline...\n"
-         << endl;
     int C, H, O;
-    cout << "Tedade Carbon: " << endl;
+    cin.exceptions(ios_base::failbit);
     try
     {
         cin >> C;
     }
     catch (const std::ios_base::failure &failure)
     {
-        cout << "Alan Mifrestimon Hava AmoAtar, Tarkib Ghalat!" << endl;
+        cout << "Boro Boro Biron Amo Atar k Alan Mirim Ro Hava!!!" << endl;
     }
-    cout << "Tedade Hydrogen: " << endl;
+    cin.exceptions(ios_base::failbit);
     try
     {
         cin >> H;
     }
     catch (const std::ios_base::failure &failure)
     {
-        cout << "Alan Mifrestimon Hava AmoAtar, Tarkib Ghalat!" << endl;
+        cout << "Boro Boro Biron Amo Atar k Alan Mirim Ro Hava!!!" << endl;
     }
-    cout << "Tedade Oxygen: " << endl;
+    cin.exceptions(ios_base::failbit);
     try
     {
         cin >> O;
     }
     catch (const std::ios_base::failure &failure)
     {
-        cout << "Alan Mifrestimon Hava AmoAtar, Tarkib Ghalat!" << endl;
+        cout << "Boro Boro Biron Amo Atar k Alan Mirim Ro Hava!!!" << endl;
     }
     Check_Naphtaline_inputs(C, H, O, amo_atar);
 }
@@ -153,8 +137,7 @@ bool Labratory::Check_Naphtaline_inputs(int C, int H, int O, Atar &amo_atar)
 {
     if (C == 10 && H == 8 && O == 0)
     {
-        cout << "\t\t Payane Farayand!" << endl;
-        cout << "Shoma Yek Pack Naphtaline Dorost Kardid." << endl;
+        cout << "Shoma Yek Vahed Naphtaline Dorost Kardid." << endl;
         //Amount increament in Inventory
         amo_atar.set_repository_amount("Naphtaline", amo_atar.get_repository_amount("Naphtaline") + 1);
 
@@ -162,7 +145,7 @@ bool Labratory::Check_Naphtaline_inputs(int C, int H, int O, Atar &amo_atar)
     }
     else
     {
-        cout << "Tarkibe Eshtebah!\n Dobare Talash Konid!" << endl;
+        cout << "Tarkibe Eshtebah! Dobare Talash Konid!" << endl;
         return false;
     }
 }
@@ -171,12 +154,9 @@ void Labratory::Initialize_Labratory(bool Lab, Atar &amo_atar)
 {
     if (Lab == true)
     {
-        cout << "\n\t\t*------*[Labratory]*------*" << endl;
-
         bool loop = true;
         while (loop == true)
         {
-            cout << "\n(1)Alcohol | (2)Kafoor | (3)Naphtaline | (0)Back To Menu" << endl;
             int choice;
             cin >> choice;
             switch (choice)
@@ -194,13 +174,12 @@ void Labratory::Initialize_Labratory(bool Lab, Atar &amo_atar)
                 loop = false;
                 break;
             default:
-                cout << "Vorodiye Eshtebah!\nTry Again:\n"
-                     << endl;
+                cout << "Vorodi Eshtebah!Dobare Talash Konid." << endl;
             }
         }
     }
     else
     {
-        cout << "Labratory Hanoz Kharidari Nashode!" << endl;
+        // cout << "Labratory Hanoz Kharidari Nashode!" << endl;
     }
 }
